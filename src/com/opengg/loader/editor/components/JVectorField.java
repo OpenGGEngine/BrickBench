860527ecd7018f8d06e22576bfa145c2819631cc
WarrenXiaGG <rlaterson@gmail.com>
2025-06-15 16:26:46 -0700

    Add copy paste to vector fields

--- src/com/opengg/loader/editor/components/JVectorField.java
+++ src/com/opengg/loader/editor/components/JVectorField.java
@@ -5,12 +5,35 @@ import com.opengg.loader.editor.tabs.EditorPane;
 
 import javax.swing.*;
 import javax.swing.text.AbstractDocument;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
 import java.awt.*;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
+import java.awt.datatransfer.DataFlavor;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JVectorField extends JComponent {
+    private static final String FLOAT_PATTERN = "(-?\\d*\\.?\\d*(E(-?)(\\d+)*)?)";
+    private static final String VECTOR_PATTERN = "\\s?" + FLOAT_PATTERN + ",\\s" + FLOAT_PATTERN + ",\\s" + FLOAT_PATTERN + "\\s?";
+
+    public static DocumentFilter floatFilter = new DocumentFilter(){
+        private final static Pattern regEx = Pattern.compile("^"+FLOAT_PATTERN+"$");
+
+        @Override
+        public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
+            Matcher matcher = regEx.matcher(text);
+            if(!matcher.matches()){
+                return;
+            }
+            super.replace(fb, offset, length, text, attrs);
+        }
+    };
+
     private Vector3f value;
     private JTextField x, y, z;
 
@@ -25,12 +48,20 @@ public class JVectorField extends JComponent {
     public JVectorField(Vector3f value, String name){
         this.value = value;
 
+        JPopupMenu rightClickPopup = makeActionMenu();
+
+        this.setComponentPopupMenu(rightClickPopup);
+
         var layout = new GridLayout(1,3);
         this.setLayout(layout);
 
         x = new JTextField(String.valueOf(value.x));
         y = new JTextField(String.valueOf(value.y));
         z = new JTextField(String.valueOf(value.z));
+
+        x.setInheritsPopupMenu(true);
+        y.setInheritsPopupMenu(true);
+        z.setInheritsPopupMenu(true);
 
         x.setColumns(6);
         y.setColumns(6);
@@ -40,9 +71,9 @@ public class JVectorField extends JComponent {
         y.setMargin(new Insets(3,0,3,0));
         z.setMargin(new Insets(3,0,3,2));
 
-        ((AbstractDocument)x.getDocument()).setDocumentFilter(EditorPane.floatFilter);
-        ((AbstractDocument)y.getDocument()).setDocumentFilter(EditorPane.floatFilter);
-        ((AbstractDocument)z.getDocument()).setDocumentFilter(EditorPane.floatFilter);
+        ((AbstractDocument)x.getDocument()).setDocumentFilter(floatFilter);
+        ((AbstractDocument)y.getDocument()).setDocumentFilter(floatFilter);
+        ((AbstractDocument)z.getDocument()).setDocumentFilter(floatFilter);
 
         ActionListener onEdit = a ->{
             this.value = new Vector3f(Float.parseFloat(x.getText()), Float.parseFloat(y.getText()), Float.parseFloat(z.getText()));
@@ -60,6 +91,47 @@ public class JVectorField extends JComponent {
         this.add(x);
         this.add(y);
         this.add(z);
+    }
+
+    private JPopupMenu makeActionMenu() {
+        JPopupMenu rightClickPopup = new JPopupMenu();
+
+        JMenuItem copyMenu = new JMenuItem("Copy Vector Field");
+        copyMenu.addActionListener(e -> {
+            try {
+                Toolkit.getDefaultToolkit().getSystemClipboard().setContents(
+                        new StringSelection(this.value.toString()),
+                        null
+                );
+            } catch (Exception ignored) {
+
+            }
+        });
+
+        JMenuItem pasteMenu = new JMenuItem("Paste Vector Field");
+        pasteMenu.addActionListener(e -> {
+            Vector3f newValue = this.value;
+            boolean success = false;
+            try {
+                String data = (String) Toolkit.getDefaultToolkit()
+                        .getSystemClipboard().getData(DataFlavor.stringFlavor);
+                if(data.matches(VECTOR_PATTERN)){
+                    data = data.trim();
+                    String[] tokens = data.split(",");
+                    newValue = new Vector3f(Float.parseFloat(tokens[0].strip()),
+                            Float.parseFloat(tokens[1].strip()), Float.parseFloat(tokens[2].strip()));
+                    success = true;
+                }
+            } catch (Exception ignored) {
+            }
+            if(success){
+                setValue(newValue);
+            }
+        });
+
+        rightClickPopup.add(copyMenu);
+        rightClickPopup.add(pasteMenu);
+        return rightClickPopup;
     }
 
     public void setValue(Vector3f value){
